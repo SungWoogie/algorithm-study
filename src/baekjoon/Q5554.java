2725bb92b624b371150363c049bc0711a9b9983d
SungWook Choi <choisw950928@naver.com>
2021-11-16 22:14:30 +0900

    <심부름 가는길> 2021.11.16

---
+++ src/baekjoon/Q5554.java
@@ -0,0 +1,22 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Q5554 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int d = scanner.nextInt();
+
+        int total = a + b + c + d;
+        int divide = total / 60;
+        int multiply = divide * 60;
+        int minus = total - multiply;
+
+        System.out.println(divide);
+        System.out.println(minus);
+    }
+}
