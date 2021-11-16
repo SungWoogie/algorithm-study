910abfc89258630b229f2cfa7c8d793dd4642b00
SungWook Choi <choisw950928@naver.com>
2021-11-16 11:30:21 +0900

    <등록> 2021.11.16

---
+++ src/baekjoon/Q7287.java
@@ -0,0 +1,8 @@
+package baekjoon;
+//2021.11.16
+public class Q7287 {
+    public static void main(String[] args) {
+        System.out.println("98");
+        System.out.println("csw950928");
+    }
+}
