3b895f6b065b7eb9ca8253afc7d95fa634f5c72f
SungWook Choi <choisw950928@naver.com>
2021-11-16 22:19:34 +0900

    <심부름 가는길> 2021.11.16 - review

---
+++ src/baekjoon/Q5554_review.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Q5554_review {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int d = scanner.nextInt();
+
+        int total = a + b + c + d;
+        int minute = total / 60;
+        int seconds = total % 60;
+
+        System.out.println(minute);
+        System.out.println(seconds);
+    }
+}
