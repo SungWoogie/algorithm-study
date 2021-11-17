fd29b46923e5024a234d3f05960ff396707f03e3
SungWook Choi <choisw950928@naver.com>
2021-11-17 11:40:25 +0900

    <A+B-2> 2021.11.17

---
+++ src/baekjoon/Q2558.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Q2558 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(a + b);
+    }
+}
