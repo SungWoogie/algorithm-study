7953c86a5c7c70be9d52115d0bc1a98da55c45d2
SungWook Choi <choisw950928@naver.com>
2021-11-23 00:25:21 +0900

    <준성이 반복문 test>2021.11.22

---
+++ src/test/JunSeong.java
@@ -0,0 +1,23 @@
+package test;
+
+import java.util.Arrays;
+
+public class JunSeong {
+    public static void main(String[] args) {
+        int[] arr = {10, 22, 34, 24, 15, 66, 87, 38, 19, 20};
+        reverse(arr);
+
+    }
+
+    public static void reverse(int[] array) {
+        int i = 0;
+        while (i < 10) {
+            i += 2;
+        }
+
+//        System.out.println(i);
+//        배열 뒤집기
+//        파라미터 배열을 뒤집는거야
+//        새로운 배열에 뒤집은 값 넣는것
+    }
+}
