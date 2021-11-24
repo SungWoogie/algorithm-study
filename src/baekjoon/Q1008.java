f3889f91bd4516617ee3af2b88297a1665f8193b
SungWook Choi <choisw950928@naver.com>
2021-11-24 17:43:50 +0900

    <A/B> 2021.11.24 - double 형

---
+++ src/baekjoon/Q1008.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Q1008 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+        System.out.println(a / b);
+    }
+}
