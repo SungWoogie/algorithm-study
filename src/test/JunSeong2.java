78c2e0571c9f1c4dbeb449dd7b41013a235848c1
SungWook Choi <choisw950928@naver.com>
2021-11-24 11:03:40 +0900

    <JunSeongTest> 2021.11.24

--- src/test/JunSeong2.java
+++ src/test/JunSeong2.java
@@ -6,24 +6,32 @@ import java.util.Scanner;
 public class JunSeong2 {
     public static void main(String[] args) {
         int[] arr = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        int[] arr1 = new int[]{10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
 
-        reverse(arr);
+        for (int i : reverse(arr)) {
+            System.out.print(i + " ");
+        }
         System.out.println();
-        newReverse(arr, arr1);
+
+        for (int i : newReverse(arr)) {
+            System.out.print(i + " ");
+        }
     }
 
     public static int[] reverse(int[] array) { // 파라미터로 배열받아서 리턴
-        for (int i = array.length - 1; i >= 0; i--) {
-            System.out.print(array[i]+" ");
+        for (int i = 0; i < array.length / 2; i++) {
+            int temp = array[i];
+            array[i] = array[array.length - i - 1];
+            array[array.length - i - 1] = temp;
         }
         return array;
     }
 
-    public static int[] newReverse(int[] array, int[] array1) { // 배열 받아서 새로운 배열로 값 뒤집어서 리턴
-        for (int i = array.length - 1; i >= 0; i--) {
-            array1[i] = array[i];
-            System.out.print(array1[i]+" ");
+    public static int[] newReverse(int[] array) { // 배열 받아서 새로운 배열로 값 뒤집어서 리턴
+        int[] array1 = new int[10];
+
+        for (int i = 0; i < array.length; i++) {
+            int temp = array[i];
+            array1[i] = temp;
         }
         return array1;
     }
