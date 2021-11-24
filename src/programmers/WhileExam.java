e0ea88c1b68d4c417eac771447df039b63a39716
SungWook Choi <choisw950928@naver.com>
2021-11-24 18:16:48 +0900

    <WhileExam> 2021.11.24

---
+++ src/programmers/WhileExam.java
@@ -0,0 +1,17 @@
+package programmers;
+
+public class WhileExam {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 10) {
+            i++;
+            System.out.println(i);
+        }
+
+        int j = 0;
+        while (j <= 10) {
+            System.out.println(j);
+            j++;
+        }
+    }
+}
