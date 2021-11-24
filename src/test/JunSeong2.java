83f3c46e4df06b5169a20211bbcf75a8ab70f192
SungWook Choi <choisw950928@naver.com>
2021-11-24 15:18:12 +0900

    <배열 역순 출력> 2021.11.24

--- src/test/JunSeong2.java
+++ src/test/JunSeong2.java
@@ -1,29 +1,28 @@
 package test;
 
-import java.util.Scanner;
-
 //2021.11.23 배열 거꾸로 출력
 public class JunSeong2 {
     public static void main(String[] args) {
         int[] arr = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-        for (int i : reverse(arr)) {
+        reverse(arr);
+        for (int i : arr) {
             System.out.print(i + " ");
         }
-        System.out.println();
+
+        System.out.println(); // 줄바꿈
 
         for (int i : newReverse(arr)) {
             System.out.print(i + " ");
         }
     }
 
-    public static int[] reverse(int[] array) { // 파라미터로 배열받아서 리턴
+    public static void reverse(int[] array) { // void 로 바꾸고 역순으로 출력
         for (int i = 0; i < array.length / 2; i++) {
             int temp = array[i];
             array[i] = array[array.length - i - 1];
             array[array.length - i - 1] = temp;
         }
-        return array;
     }
 
     public static int[] newReverse(int[] array) { // 배열 받아서 새로운 배열로 값 뒤집어서 리턴
