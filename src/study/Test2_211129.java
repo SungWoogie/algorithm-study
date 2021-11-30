10fc0dbbd9e936f06e2437ee3eeaa815b544ab1a
SungWook Choi <choisw950928@naver.com>
2021-11-30 17:02:51 +0900

    <공통 NumberRemove> 2021.11.30 - 배열에 들어있는 공통 숫자 삭제 하고 리턴

--- src/study/Test2_211129.java
+++ src/study/Test2_211129.java
@@ -2,20 +2,52 @@ package study;
 
 public class Test2_211129 {
     public static void main(String[] args) {
-        int[] arr = new int[]{10, 80, 7, 10, 7, 5, 20, 35, 70, 15, 10, 3};
-        int[] post = Array(arr);
+        int[] arr = new int[]{10, 10, 20, 30, 20};
+//        int[] post = Array(arr);
+        int[] remove = remove(arr);
 
-        for (int a : post) {
+        for (int a : remove) {
             System.out.print(a + " ");
         }
+
+//        for (int a : post) {
+//            System.out.print(a + " ");
+//        }
     }
 
-    public static int[] Array(int[] array) {
-        int[] returnArr = new int[array.length];
+//    public static int[] Array(int[] array) {
+//        int[] returnArr = new int[array.length];
+//
+//        for (int i = 0; i < array.length; i++) {
+//            returnArr[array.length - i - 1] = array[i];
+//        }
+//        return returnArr;
+//    }
 
-        for (int i = 0; i < array.length; i++) {
-            returnArr[array.length - i - 1] = array[i];
+    public static int[] remove(int[] arr) {
+
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            int num = arr[i];
+            for (int j = i + 1; j < arr.length; j++) {
+                if (num == arr[j]) {
+                    count++;
+                    arr[i] = 0;
+                }
+            }
         }
-        return returnArr;
+
+        int[] newArr = new int[arr.length - count];
+        count = 0;
+        for (int j : arr) {
+            if (j != 0) {
+                newArr[count] = j;
+                count++;
+            }
+        }
+
+        return newArr;
     }
 }
+
