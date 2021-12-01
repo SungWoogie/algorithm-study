55bf3fe4e43c2dcb17eb1b1ed67d67c07dfd2f21
SungWook Choi <choisw950928@naver.com>
2021-12-01 12:10:20 +0900

    <Add> 2021.12.01 - ArrayList add 메소드 구현

---
+++ src/test/SungJun3_211201.java
@@ -0,0 +1,23 @@
+package test;
+
+import java.util.Arrays;
+
+public class SungJun3_211201 {
+    public static void main(String[] args) {
+        int num = 1000;
+        int[] arr = new int[]{50, 60, 70, 10, 20, 30, 50, 60, 70, 80, 20};
+        int[] newArray = add(arr, num);
+
+        System.out.println(Arrays.toString(newArray));
+    }
+
+    public static int[] add(int[] arr, int num) {
+        int[] returnArr = new int[arr.length + 1];
+
+        for (int i = 0; i < arr.length; i++) {
+            returnArr[i] = arr[i];
+        }
+        returnArr[returnArr.length - 1] = num;
+        return returnArr;
+    }
+}
