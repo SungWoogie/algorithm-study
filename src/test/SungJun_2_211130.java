044bf5ec7cbe094e751214c60837aa0ae0c48cfe
SungWook Choi <choisw950928@naver.com>
2021-12-01 11:53:25 +0900

    <indexAdd> 2021.12.01 - ArrayList add(index, num) 메소드 구현 - count 활용

--- src/test/SungJun_2_211130.java
+++ src/test/SungJun_2_211130.java
@@ -2,22 +2,27 @@ package test;
 
 public class SungJun_2_211130 {
     public static void main(String[] args) {
-        int index = 0;
-        int[] arr = new int[]{2, 7, 3, 1, 5};
-        int[] indexAdd = indexAdd(arr, index);
+        int index = 2;
+        int num = 1000;
+        int[] arr = new int[]{10, 30, 50, 80, 90};
 
-
+        for (int a : indexAdd(arr, index, num)) {
+            System.out.print(a + " ");
+        }
     }
 
-    public static int[] indexAdd(int[] arr, int index) {
-        int[] tempArray = new int[arr.length + 1]; // 기존 배열보다 한개 더 많은 배열 생성
+    public static int[] indexAdd(int[] arr, int index, int num) {
+        int[] returnArray = new int[arr.length + 1]; // 기존 배열보다 한개 더 많은 배열 생성
 
-        for (int i = 0; i < tempArray.length; i++) {
+        int count = 0; // arr 의 크기만큼만 돌기위해 만든 카운트
+        for (int i = 0; i < returnArray.length; i++) { //i는 returnArray 의 크기만큼 돌기위한 변수
             if (index == i) {
-                tempArray[i] = tempArray[index];
+                returnArray[index] = num;
+            } else {
+                returnArray[i] = arr[count];
+                count++;
             }
         }
-
-        return null;
+        return returnArray;
     }
 }
