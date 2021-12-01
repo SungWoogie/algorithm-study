a4d0624e0eef193b4004885edffb9b76dad10212
SungWook Choi <choisw950928@naver.com>
2021-12-01 13:50:33 +0900

    <remove 구현> 2021.12.01 - ArrayList remove 메소드 구현

---
+++ src/test/SungJun4_211201.java
@@ -0,0 +1,26 @@
+package test;
+
+public class SungJun4_211201 {
+    public static void main(String[] args) {
+        int index = 0;
+        int[] arr = new int[]{10, 20, 30, 40, 50, 20};
+        int[] remove = remove(arr, index);
+
+        for (int a : remove) {
+            System.out.print(a + " ");
+        }
+    }
+
+    public static int[] remove(int[] arr, int index) {
+        int[] returnArr = new int[arr.length - 1];
+
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (i != index) {
+                returnArr[count] = arr[i];
+                count++;
+            }
+        }
+        return returnArr;
+    }
+}
