c1cca807149911a702b171d37344feddfa44438d
SungWook Choi <choisw950928@naver.com>
2021-12-02 11:18:21 +0900

    <직사각형 별찍기> 2021.12.02 - 프로그래머스 연습문제

---
+++ src/programmers/Challenger_2.java
@@ -0,0 +1,19 @@
+package programmers;
+
+import java.util.Scanner;
+
+public class Challenger_2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
