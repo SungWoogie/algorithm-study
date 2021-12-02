15cb55c4569835ae39d91b822f037d5ec224940b
SungWook Choi <choisw950928@naver.com>
2021-12-02 15:24:41 +0900

    <whileTest> 2021.12.02

---
+++ src/test/test_211202.java
@@ -0,0 +1,19 @@
+package test;
+
+public class test_211202 {
+    public static void main(String[] args) {
+        int x = 222;
+
+        int percent = 0;
+        int divide = 0;
+
+        while (10 < x) {
+            percent = x % 10;
+            divide = x / 10;
+            x = divide;
+
+            System.out.println(x);
+        }
+
+    }
+}
