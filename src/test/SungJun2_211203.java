609b9308b005a0d324d270c3099cf69fc3d52bc5
SungWook Choi <choisw950928@naver.com>
2021-12-03 16:36:13 +0900

    <indexAdd 연습> 2021.12.03

---
+++ src/test/SungJun2_211203.java
@@ -0,0 +1,30 @@
+package test;
+
+public class SungJun2_211203 {
+    public static void main(String[] args) {
+        int index = 3;
+        int num = 100;
+        int[] arr = new int[]{20, 30, 40, 50, 60};
+        int[] returnArr = indexAdd(arr, index, num);
+
+        for (int a : returnArr){
+            System.out.print(a + " ");
+        }
+    }
+
+    public static int[] indexAdd(int[] arr, int index, int num) {
+        int[] returnArr = new int[arr.length + 1];
+
+        int count = 0;
+
+        for (int i = 0; i < returnArr.length; i++) {
+            if (index == i) {
+                returnArr[index] = num;
+            } else {
+                returnArr[i] = arr[count];
+                count++;
+            }
+        }
+        return returnArr;
+    }
+}
