d1e3363317e748278d85cd293e71a1f7818ac39f
SungWook Choi <choisw950928@naver.com>
2021-12-03 13:47:05 +0900

    <add 연습> 2021.12.03

---
+++ src/test/SungJun1_211203.java
@@ -0,0 +1,24 @@
+package test;
+
+public class SungJun1_211203 {
+    public static void main(String[] args) {
+        int num = 1000;
+        int[] arr = new int[]{20, 30, 40, 50, 70};
+        int[] returnArr = add(arr, num);
+
+        for (int a : returnArr) {
+            System.out.print(a + " ");
+        }
+    }
+
+    public static int[] add(int[] arr, int num) {
+        int[] newArr = new int[arr.length + 1];
+
+        for (int i = 0; i < arr.length; i++) {
+            newArr[i] = arr[i];
+        }
+        newArr[newArr.length - 1] = num;
+
+        return newArr;
+    }
+}
