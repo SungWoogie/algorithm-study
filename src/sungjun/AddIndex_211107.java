98becc59c28b83a84a962160b18292bac078f5ff
SungWook Choi <choisw950928@naver.com>
2021-12-07 01:16:05 +0900

    <addIndex 구현>  2021.12.07 - 손코딩 연습 후 컴파일 실행 연습 - fail

---
+++ src/sungjun/AddIndex_211107.java
@@ -0,0 +1,27 @@
+package sungjun;
+
+public class AddIndex_211107 {
+    public static void main(String[] args) {
+        int[] arr = new int[]{77, 11, 22, 0, 55, 99};
+        int index = 3;
+        int num = 1000;
+        int[] result = solution(arr, index, num);
+        for (int a : result) {
+            System.out.print(a + " ");
+        }
+    }
+
+    public static int[] solution(int[] arr, int index, int num) {
+        int[] newArr = new int[arr.length + 1];
+        int count = 0;
+
+        for (int i = 0; i < newArr.length; i++) {
+            if (arr[i] != index) {
+                newArr[i] = arr[i];
+                count++;
+            }
+            newArr[count] = arr[num];
+        }
+        return newArr;
+    }
+}
