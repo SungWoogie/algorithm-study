fff46283b767b36683a024654321b102d173a541
SungWook Choi <choisw950928@naver.com>
2021-12-06 19:43:33 +0900

    <JunSeong ArrayList 구현>  2021.12.06

--- src/test/JunSeong.java
+++ src/test/JunSeong.java
@@ -1,23 +1,67 @@
 package test;
 
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class JunSeong {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         int[] arr = {10, 22, 34, 24, 15, 66, 87, 38, 19, 20};
-        reverse(arr);
 
+//        while (true) { // add 실행문
+//            int num = scanner.nextInt();
+//            arr = add(arr, num);
+//            print(arr);
+//        }
+        while (true) { // indexAdd 실행문
+            int index = scanner.nextInt();
+            int num = scanner.nextInt();
+            arr = indexAdd(arr, index, num);
+            print(arr);
+        }
     }
 
-    public static void reverse(int[] array) {
-        int i = 0;
-        while (i < 10) {
-            i += 2;
-        }
 
-//        System.out.println(i);
-//        배열 뒤집기
-//        파라미터 배열을 뒤집는거야
-//        새로운 배열에 뒤집은 값 넣는것
+    public static int[] add(int[] arr, int num) {
+        int[] array = new int[arr.length + 1];
+        for (int i = 0; i < arr.length; i++) {
+            array[i] = arr[i];
+        }
+        array[array.length - 1] = num;
+        arr = array;
+
+        return arr;
+    }
+
+    public static int[] indexAdd(int[] arr, int index, int num) {
+        int[] newArray = new int[arr.length + 1];
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            newArray[i] = arr[i]; // 구 배열을, 뉴 배열에 값 옮기기
+            if (newArray[i] != newArray[index])
+                count++;{
+            }
+        }
+        //쥰로직
+        count = 0;
+        for (int i = 0; i < newArray.length; i++) { //arr3개 값을 가지고있다 10 20 30
+            if (i == index) {
+                newArray[i] = num; //값 1개 추가       총newarr에 값을 4개 추가해줘야함
+            } else {
+                newArray[i] = arr[count];
+                count++;
+            }
+        }
+        //끝
+
+
+        newArray[count] = num;
+        arr = newArray;
+        return arr;
+    }
+
+    public static void print(int[] arr1) {
+        for (int a : arr1) {
+            System.out.print(a + " ");
+        }
     }
 }
