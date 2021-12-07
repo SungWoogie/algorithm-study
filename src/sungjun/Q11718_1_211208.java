54006b125564d7f0f2d1e41404ba2f536df64be0
SungWook Choi <choisw950928@naver.com>
2021-12-08 01:59:56 +0900

    <Q11718 그대로 출력하기>  2021.12.08 - hasNext

---
+++ src/sungjun/Q11718_1_211208.java
@@ -0,0 +1,13 @@
+package sungjun;
+
+import java.util.Scanner;
+
+public class Q11718_1_211208 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            String a = scanner.nextLine();
+            System.out.println(a);
+        }
+    }
+}
