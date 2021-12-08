468b5d44aa42f89eb7845d745599b6b98a66c8e7
SungWook Choi <choisw950928@naver.com>
2021-12-09 02:05:01 +0900

    <Q11718 그대로 출력하기> 2021.12.08

---
+++ src/baekjoon/Q11718.java
@@ -0,0 +1,14 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Q11718 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNext()){
+            String str = scanner.nextLine();
+            System.out.println(str);
+        }
+    }
+}
