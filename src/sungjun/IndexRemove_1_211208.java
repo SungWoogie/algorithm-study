170941f9b7e5d679026081c85997b6c75e121030
SungWook Choi <choisw950928@naver.com>
2021-12-09 02:06:44 +0900

    <SungJun4> 2021.12.08 - IndexRemove

---
+++ src/sungjun/IndexRemove_1_211208.java
@@ -0,0 +1,26 @@
+package sungjun;
+
+public class IndexRemove_1_211208 {
+    public static void main(String[] args) {
+        int index = 1;
+        int[] arr = new int[]{20, 10, 50, 100, 30};
+        int[] remove = remove(arr, index);
+
+        for (int a : remove) {
+            System.out.print(a + " ");
+        }
+    }
+
+    public static int[] remove(int[] arr, int index) {
+        int[] newArr = new int[arr.length - 1];
+
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (i != index) {
+                newArr[count] = arr[i];
+                count++;
+            }
+        }
+        return newArr;
+    }
+}
