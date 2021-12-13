46d2f8760554bf105dbdb35fde1fe480d49b1f5f
SungWook Choi <choisw950928@naver.com>
2021-12-13 21:00:20 +0900

    <Q16430 제리와 톰> 2021.12.13 - bronze5

---
+++ src/baekjoon/Q16430.java
@@ -0,0 +1,14 @@
+package baekjoon;
+// 21.12.13
+import java.util.Scanner;
+
+public class Q16430 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println(b - a + " " + b);
+    }
+}
