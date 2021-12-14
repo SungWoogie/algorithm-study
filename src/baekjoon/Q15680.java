b9f23f0b0eff2c384fb9614891c1f97ec6b35b74
SungWook Choi <choisw950928@naver.com>
2021-12-14 19:03:25 +0900

    <연세대학교> 2021.12.14 - easy

---
+++ src/baekjoon/Q15680.java
@@ -0,0 +1,18 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Q15680 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        if (n == 0) {
+            System.out.println("YONSEI");
+        } else {
+            System.out.println("Leading the Way to the Future");
+        }
+    }
+}
+
