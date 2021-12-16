ae1017f71fc1cf68bd10cb4ca60b9f039e40d007
SungWook Choi <choisw950928@naver.com>
2021-12-16 18:19:55 +0900

    <Q10430 나머지> 2021.12.16 - 소괄호 활용

---
+++ src/review/Q10430_Review.java
@@ -0,0 +1,18 @@
+package review;
+
+import java.util.Scanner;
+
+public class Q10430_Review {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+        System.out.println((a + b) % c);
+        System.out.println(((a % c) + (b % c)) % c);
+        System.out.println((a * b) % c);
+        System.out.println(((a % c) * (b % c)) % c);
+    }
+}
