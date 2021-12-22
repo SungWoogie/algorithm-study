0a3ad03773f4876798b96912f2e0896d09e86769
SungWook Choi <choisw950928@naver.com>
2021-12-22 11:03:42 +0900

    <Q2445 별 찍기-8> 2021.12.22 - 다시

---
+++ src/baekjoon/Q2445.java
@@ -0,0 +1,36 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Q2445 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            for (int j = n * 2; j > i * 2; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = n - 1; i > 0; i--) {
+            for (int j = i; j > 0; j--) {
+                System.out.print("*");
+            }
+            for (int j = i * 2; j < n * 2; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
