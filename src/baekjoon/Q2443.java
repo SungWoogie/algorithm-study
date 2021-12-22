dc002857b7331a8d3a2e94751a390e775cda7575
SungWook Choi <choisw950928@naver.com>
2021-12-22 11:04:59 +0900

    <Q2443 별 찍기-6> 2021.12.22 - 다시

--- src/baekjoon/Q2443.java
+++ src/baekjoon/Q2443.java
@@ -9,11 +9,11 @@ public class Q2443 {
         int n = scanner.nextInt();
 
         for (int i = 1; i <= n; i++) {
-            for (int j = i; j <= 2 * n - i; j++) {
-                System.out.print("*");
-            }
             for (int j = 0; j < i - 1; j++) {
                 System.out.print(" ");
+            }
+            for (int j = i; j <= 2 * n - i; j++) {
+                System.out.print("*");
             }
             System.out.println();
         }
