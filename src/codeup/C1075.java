2cb669a9ff14e36b31254182351f84aa351d6ac2
SungWook Choi <choisw950928@naver.com>
2021-12-26 23:47:56 +0900

    <C1075 정수 1개 입력받아 카운트 다운 출력2> 2021.12.26

---
+++ src/codeup/C1075.java
@@ -0,0 +1,15 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class C1075 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        for (int i = n - 1; i >= 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
