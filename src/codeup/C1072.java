6ccf79a1f2a0d8b05baea5761d81a4d1cb8a2a41
SungWook Choi <choisw950928@naver.com>
2021-12-26 23:32:12 +0900

    <C1072 정수 입력받아 계속 출력하기> 2021.12.26

---
+++ src/codeup/C1072.java
@@ -0,0 +1,16 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class C1072 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int num = scanner.nextInt();
+            System.out.println(num);
+        }
+    }
+}
