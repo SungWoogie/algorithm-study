c16222092d5fa04288abbbaf0c9f9e89bc4fe423
SungWook Choi <choisw950928@naver.com>
2021-12-26 23:40:32 +0900

    <C1074 정수 1개 입력받아 카운트 다운 출력> 2021.12.26

---
+++ src/codeup/C1074.java
@@ -0,0 +1,16 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class C1074 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num = scanner.nextInt();
+
+        while (num != 0) {
+            System.out.println(num);
+            num--;
+        }
+    }
+}
