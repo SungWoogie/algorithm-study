d8854ebf0db016419e81cc212c230e56d190f438
SungWook Choi <choisw950928@naver.com>
2021-12-27 17:27:45 +0900

    <C1083 3 6 9 게임의 왕이 되자!> 2021.12.27

---
+++ src/codeup/C1083.java
@@ -0,0 +1,18 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class C1083 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+
+        for (int i = 1; i <= num; i++) {
+            if (i % 3 == 0) {
+                System.out.print('X' + " ");
+            } else {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
