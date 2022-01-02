f0916df959b8db8167251b5e35f1a6142926c81d
SungWook Choi <choisw950928@naver.com>
2022-01-02 15:12:53 +0900

    <C1251> 2022.01.02 - 1 부터 100까지 출력하기 - 단순 반복문

---
+++ src/codeup/C1251.java
@@ -0,0 +1,12 @@
+package codeup;
+
+public class C1251 {
+    public static void main(String[] args) {
+
+        int i = 1;
+        while (i <= 100) {
+            System.out.print(i + " ");
+            i++;
+        }
+    }
+}
