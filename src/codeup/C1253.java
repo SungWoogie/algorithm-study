d20077cab5231b903beda54a869a0fb0449b9415
SungWook Choi <choisw950928@naver.com>
2022-01-05 16:54:37 +0900

    <C1253> 2022.01.05 - a 부터 b 까지 출력하기 - 단순 반복문 - fail (다시)

---
+++ src/codeup/C1253.java
@@ -0,0 +1,26 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class C1253 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        if (a < b) {
+            while (a <= b) {
+                System.out.print(a + " ");
+                a++;
+            }
+        } else if (b < a) {
+            while (b <= a) {
+                System.out.print(b + " ");
+                b++;
+            }
+        } else {
+            System.out.println(a);
+        }
+    }
+}
