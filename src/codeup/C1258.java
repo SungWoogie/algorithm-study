33e3a039779848f64f0bfffff33aa305b19cddd5
SungWook Choi <choisw950928@naver.com>
2022-01-06 14:25:58 +0900

    <C1258> 2022.01.06 - 1부터 n까지 합 구하기 - 단순 반복문

---
+++ src/codeup/C1258.java
@@ -0,0 +1,17 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class C1258 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
