a8f4760e57609003be2883f287daf68324b24be2
SungWook Choi <choisw950928@naver.com>
2022-01-10 22:11:41 +0900

    <C1106> 2022.01.10 - int 범위 출력

---
+++ src/codeup/C1106.java
@@ -0,0 +1,9 @@
+package codeup;
+
+public class C1106 {
+    public static void main(String[] args) {
+        int a = -2147483648;
+        int b = 2147483647;
+        System.out.printf("%d %d", a, b);
+    }
+}
