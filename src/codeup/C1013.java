3c491edebec4bee036138abd842dabe67f5e0fff
SungWook Choi <choisw950928@naver.com>
2022-01-11 14:17:40 +0900

    <C1013> 2022.01.11 - 정수 2개 입력받아 그대로 출력하기

---
+++ src/codeup/C1013.java
@@ -0,0 +1,10 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class C1013 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.nextInt() + " " + scanner.nextInt());
+    }
+}
