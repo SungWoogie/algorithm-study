fb4c90be3698334de0a753dd7184e60d9c577079
SungWook Choi <choisw950928@naver.com>
2022-01-11 15:00:42 +0900

    <C1023> 2022.01.11 - 실수 1개 입력받아 부분별로 출력하기

---
+++ src/codeup/C1023.java
@@ -0,0 +1,12 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class C1023 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String str = scanner.next().replace(".","\n");
+        System.out.println(str);
+    }
+}
