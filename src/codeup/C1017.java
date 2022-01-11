8d1d9c2a88374812bbae2e3a59ecff7b5e2d0f6e
SungWook Choi <choisw950928@naver.com>
2022-01-11 14:24:55 +0900

    <C1017> 2022.01.11 - 정수 1개 입력받아 3번 출력하기

---
+++ src/codeup/C1017.java
@@ -0,0 +1,14 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class C1017 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        for (int i = 0; i < 3; i++) {
+            System.out.print(n + " ");
+        }
+    }
+}
