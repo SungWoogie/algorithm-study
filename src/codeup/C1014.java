7e184402462e9f8bf4ad87c38862e508683c034b
SungWook Choi <choisw950928@naver.com>
2022-01-11 14:22:16 +0900

    <C1014> 2022.01.11 - 정수 2개 입력받아 순서 바꿔 출력하기

---
+++ src/codeup/C1014.java
@@ -0,0 +1,13 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class C1014 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String a = scanner.next();
+        String b = scanner.next();
+        System.out.println(b + " " + a);
+    }
+}
