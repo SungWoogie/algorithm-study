951bf4269ba172778dc71b789d0114658811732e
SungWook Choi <choisw950928@naver.com>
2022-01-11 14:15:58 +0900

    <C1012> 2022.01.11 - 실수 1개 입력받아 그대로 출력하기

---
+++ src/codeup/C1012.java
@@ -0,0 +1,12 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class C1012 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        float f = scanner.nextFloat();
+        System.out.printf("%f", f);
+    }
+}
