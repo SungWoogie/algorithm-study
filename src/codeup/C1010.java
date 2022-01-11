44c6a881690f367a455ca2f5dbe5caec8330b395
SungWook Choi <choisw950928@naver.com>
2022-01-11 14:05:28 +0900

    <C1010> 2022.01.11 - 정수 1개 입력받아 그대로 출력하기

---
+++ src/codeup/C1010.java
@@ -0,0 +1,10 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class C1010 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.nextInt());
+    }
+}
