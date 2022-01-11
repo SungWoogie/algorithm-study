85f82c3e5221bff6918629f407be41ed822d9209
SungWook Choi <choisw950928@naver.com>
2022-01-11 14:52:29 +0900

    <C1021> 2022.01.11 - 단어 1개 입력받아 형태 그대로 출력하기

---
+++ src/codeup/C1021.java
@@ -0,0 +1,12 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class C1021 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String str = scanner.nextLine();
+        System.out.println(str);
+    }
+}
