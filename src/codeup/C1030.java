b76107f283948d142ce7c192594ff11bf37d97b5
SungWook Choi <choisw950928@naver.com>
2022-01-14 16:25:52 +0900

    <C1029> 2022.01.14 - 정수 1개 입력받아 출력하기

---
+++ src/codeup/C1030.java
@@ -0,0 +1,12 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class C1030 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        long n = scanner.nextLong();
+        System.out.println(n);
+    }
+}
