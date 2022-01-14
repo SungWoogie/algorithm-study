7bcbaf063b0442a2582b1ea737b7b9047d6afdad
SungWook Choi <choisw950928@naver.com>
2022-01-14 16:39:00 +0900

    <C1032> 2022.01.14 - 10진수 정수 -> 16진수로 변환 하여 출력하기

---
+++ src/codeup/C1032.java
@@ -0,0 +1,12 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class C1032 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num = scanner.nextInt();
+        System.out.println(Integer.toHexString(num)); // 10진수 -> 16진수로 변환 메소드
+    }
+}
