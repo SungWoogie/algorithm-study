ca0a1843aabc891ae31516739e84ed9e24fb15e3
SungWook Choi <choisw950928@naver.com>
2022-01-17 23:35:55 +0900

    <C1041> 22.01.17 - 문자 1개 입력 받아 다음 문자 출력

---
+++ src/codeup/C1041.java
@@ -0,0 +1,12 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class C1041 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        char str = (char) (scanner.next().charAt(0) + 1);
+        System.out.println(str);
+    }
+}
