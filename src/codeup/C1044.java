c838860549cabc53509db553c1f53c7c9b77a0c5
SungWook Choi <choisw950928@naver.com>
2022-01-17 23:43:25 +0900

    <C1044> 22.01.17 - easy

---
+++ src/codeup/C1044.java
@@ -0,0 +1,13 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class C1044 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        long num = scanner.nextLong();
+
+        System.out.println(num + 1);
+    }
+}
