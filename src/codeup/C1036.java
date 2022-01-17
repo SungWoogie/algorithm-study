2a87f7eeb8fcdd236b7fbaf067c88047a9349ea2
SungWook Choi <choisw950928@naver.com>
2022-01-17 23:14:11 +0900

    <C1036> 22.01.17 - 영문자 1개 입력받아 10진수로 출력

---
+++ src/codeup/C1036.java
@@ -0,0 +1,12 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class C1036 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        char str = scanner.next().charAt(0);
+        System.out.println((int) str);
+    }
+}
