84f5ae1d5b625038e5a5bc2b149c41421303cad4
SungWook Choi <choisw950928@naver.com>
2022-01-17 23:39:56 +0900

    <C1042> 22.01.17 - easy

---
+++ src/codeup/C1043.java
@@ -0,0 +1,13 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class C1043 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num = scanner.nextInt();
+        int num1 = scanner.nextInt();
+        System.out.println(num % num1);
+    }
+}
