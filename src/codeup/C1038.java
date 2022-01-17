ed3973c54c259d67bfaefe419daa17f9e4176a6c
SungWook Choi <choisw950928@naver.com>
2022-01-17 23:27:10 +0900

    <C1038> 22.01.17 - 정수 2개 입력받아 합 출력

---
+++ src/codeup/C1038.java
@@ -0,0 +1,14 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class C1038 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        long num = scanner.nextLong();
+        long num1 = scanner.nextLong();
+
+        System.out.println(num + num1);
+    }
+}
