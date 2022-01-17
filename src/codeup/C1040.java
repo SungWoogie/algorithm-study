bee4a49b989a3045c4f81761a80d259bf886bae1
SungWook Choi <choisw950928@naver.com>
2022-01-17 23:31:21 +0900

    <C1040> 22.01.17 - 정수 1개 입력 받아 부호 바꿔 출력

---
+++ src/codeup/C1040.java
@@ -0,0 +1,13 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class C1040 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num = scanner.nextInt();
+        int value = -1;
+        System.out.println(num * value);
+    }
+}
