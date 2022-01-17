75fabae66061db1016f7c72bcd2c4d87bcc5d212
SungWook Choi <choisw950928@naver.com>
2022-01-17 22:51:24 +0900

    <C1034> 22.01.17 - 8진 정수 1개 입력받아 10진수로 출력

---
+++ src/codeup/C1034.java
@@ -0,0 +1,14 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class C1034 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String n = scanner.next();
+
+        int value = Integer.valueOf(n,8);
+        System.out.println(value);
+    }
+}
