ba1c4af650744988328a5ed94d975577cca19c82
SungWook Choi <choisw950928@naver.com>
2022-01-21 16:50:48 +0900

    <C1132> 22.01.21

---
+++ src/codeup/C1132.java
@@ -0,0 +1,12 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class C1132 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String str = scanner.next();
+        System.out.println(str);
+    }
+}
