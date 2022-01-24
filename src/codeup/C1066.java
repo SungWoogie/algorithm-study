571f197e15ac6bf07fa8f7f97531b0097d2519f6
SungWook Choi <choisw950928@naver.com>
2022-01-24 18:33:28 +0900

    <C1066> 22.01.24 - if

---
+++ src/codeup/C1066.java
@@ -0,0 +1,19 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class C1066 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < 3; i++) {
+            int num = scanner.nextInt();
+
+            if (num % 2 == 0) {
+                System.out.println("even");
+            } else {
+                System.out.println("odd");
+            }
+        }
+    }
+}
