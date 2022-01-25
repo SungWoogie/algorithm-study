5dc0cb556b8ccbcfd9255d07b24aaa66dd114723
SungWook Choi <choisw950928@naver.com>
2022-01-25 15:54:36 +0900

    <C1053> 22.01.25 - if()

---
+++ src/codeup/C1053.java
@@ -0,0 +1,17 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class C1053 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+
+        if (a == 0) {
+            System.out.println(1);
+        } else if (a == 1) {
+            System.out.println(0);
+        }
+    }
+}
