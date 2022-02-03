aa9049a6c01a0bc38aecdc49839b82d0cfd8c3e5
SungWook Choi <choisw950928@naver.com>
2022-02-03 10:05:21 +0900

    <Q15552> 22.02.03 - 빠른 A+B - 시간초과로 인하여 Scanner 대신 BufferReader / println 대신 BufferedWriter 활용

---
+++ src/baekjoon/Q15552.java
@@ -0,0 +1,24 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Q15552 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            String str = br.readLine();
+            StringTokenizer st = new StringTokenizer(str);
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            int sum = a + b;
+
+            bw.write(sum + "\n");
+        }
+        bw.close();
+    }
+}
