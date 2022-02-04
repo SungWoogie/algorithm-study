e98457b04f45c50b96549a418cf2edc8a519c826
SungWook Choi <choisw950928@naver.com>
2022-02-05 00:44:01 +0900

    <Q4999 아!> 22.02.04

--- src/baekjoon/Q4999.java
+++ src/baekjoon/Q4999.java
@@ -6,10 +6,10 @@ public class Q4999 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String str = scanner.next();
         String str1 = scanner.next();
+        String str2 = scanner.next();
 
-        if (str.length() < str1.length()) {
+        if (str1.length() < str2.length()) {
             System.out.println("no");
         } else {
             System.out.println("go");
