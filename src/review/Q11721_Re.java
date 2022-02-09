2aafea7db9e519dc60d38197e6501793bd3b62cc
SungWook Choi <choisw950928@naver.com>
2022-02-09 17:31:41 +0900

    <Q11721_Re 열 개씩 끊어 출력하기(re)> 22.02.09

--- src/review/Q11721_Re.java
+++ src/review/Q11721_Re.java
@@ -3,19 +3,30 @@ package review;
 import java.util.Scanner;
 
 public class Q11721_Re {
+    //        public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        StringBuilder sb = new StringBuilder();
+//
+//        String str = scanner.next();
+//
+//        for (int i = 1; i <= str.length(); i++) {
+//            char ch = str.charAt(i - 1);
+//            sb.append(ch);
+//            if (i % 10 == 0) {
+//                sb.append("\n");
+//            }
+//        }
+//        System.out.println(sb);
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        StringBuilder sb = new StringBuilder();
 
-        String str = scanner.next();
+        String str1 = scanner.next();
 
-        for (int i = 1; i <= str.length(); i++) {
-            char ch = str.charAt(i - 1);
-            sb.append(ch);
-            if (i % 10 == 0) {
-                sb.append("\n");
+        for (int i = 0; i < str1.length(); i++) {
+            System.out.print(str1.charAt(i));
+            if (i % 10 == 9) {
+                System.out.println();
             }
         }
-        System.out.println(sb);
     }
 }
