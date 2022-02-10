5c8c71b6bbcc82485da08cb0c5b495e51c4027f8
SungWook Choi <choisw950928@naver.com>
2022-02-10 14:00:27 +0900

    <Q1157 단어 공부(fail)> 22.02.10

--- src/baekjoon/Q1157.java
+++ src/baekjoon/Q1157.java
@@ -1,24 +1,25 @@
 package baekjoon;
-
+// 22.02.10 못품
 import java.util.Scanner;
 
 public class Q1157 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int[] arr = new int[26]; // 알파벳 개수
 
-        String str = scanner.next().toUpperCase(); // 대문자 출력으로 대문자 입력
+        int[] arr = new int[26];
+        String str = scanner.next().toUpperCase();
 
-        int max = 1;
+        for (int i = 0; i < str.length(); i++) { // 몇개 나왔는지 체크
+            int num = str.charAt(i) - 65; // 대문자 - 65
+            arr[num]++;
+        }
+        int max = 0;
         char ch = '?';
-
-        for (int i = 0; i < str.length(); i++) {
-            int index = arr[str.charAt(i) - 65];
-            index++;
-            if (max < index) {
-                max = index;
-                ch = str.charAt(i);
-            } else if (max == index) {
+        for (int i = 0; i < arr.length; i++) { // 누가 제일 많이 나왔는지 체크
+            if (max < arr[i]) {
+                max = arr[i];
+                ch = (char) (i + 65);
+            } else if (max == arr[i]) {
                 ch = '?';
             }
         }
