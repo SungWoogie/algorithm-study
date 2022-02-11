cc80b7b8ffa7ba505fa6ae1946896f6ead547895
SungWook Choi <choisw950928@naver.com>
2022-02-11 15:49:07 +0900

    <Q7567 그릇(re)> 22.02.11

---
+++ src/baekjoon/Q10804.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Q10804 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String str = scanner.next();
+        int sum = 10;
+
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i - 1) == str.charAt(i)) {
+                sum += 5;
+            } else {
+                sum += 10;
+            }
+        }
+        System.out.println(sum);
+    }
+}
