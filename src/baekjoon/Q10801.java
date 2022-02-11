f7ab901e709e671c0a94dcd485da95ac4d896dab
SungWook Choi <choisw950928@naver.com>
2022-02-11 14:38:21 +0900

    <Q10801 카드게임> 22.02.11 - for 문을 한개만 사용하기 위하여 nextLine()으로 받아서 Integer.ParseInt() 형변환

--- src/baekjoon/Q10801.java
+++ src/baekjoon/Q10801.java
@@ -3,24 +3,51 @@ package baekjoon;
 import java.util.Scanner;
 
 public class Q10801 {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//
+//
+//        int[] aArr = new int[10];
+//        int[] bArr = new int[10];
+//        for (int i = 0; i < aArr.length; i++) {
+//            aArr[i] = scanner.nextInt();
+//        }
+//        for (int i = 0; i < bArr.length; i++) {
+//            bArr[i] = scanner.nextInt();
+//        }
+//        int aCount = 0;
+//        int bCount = 0;
+//
+//        for (int i = 0; i < aArr.length; i++) {
+//            if (aArr[i] > bArr[i]) {
+//                aCount++;
+//            } else if (aArr[i] < bArr[i]) {
+//                bCount++;
+//            }
+//        }
+//        if (aCount > bCount) {
+//            System.out.println("A");
+//        } else if (aCount < bCount) {
+//            System.out.println("B");
+//        } else {
+//            System.out.println("D");
+//        }
+//    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] aStr = scanner.nextLine().split(" ");
+        String[] bStr = scanner.nextLine().split(" ");
 
-        int[] aArr = new int[10];
-        int[] bArr = new int[10];
-        for (int i = 0; i < aArr.length; i++) {
-            aArr[i] = scanner.nextInt();
-        }
-        for (int i = 0; i < bArr.length; i++) {
-            bArr[i] = scanner.nextInt();
-        }
         int aCount = 0;
         int bCount = 0;
+        for (int i = 0; i < 10; i++) {
+            int aNum = Integer.parseInt(aStr[i]);
+            int bNum = Integer.parseInt(bStr[i]);
 
-        for (int i = 0; i < aArr.length; i++) {
-            if (aArr[i] > bArr[i]) {
+            if (aNum > bNum) {
                 aCount++;
-            } else if (aArr[i] < bArr[i]) {
+            } else if (aNum < bNum) {
                 bCount++;
             }
         }
