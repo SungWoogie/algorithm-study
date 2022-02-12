0ac6bff24d0b91ce7fb32c6895b83b65c94c678c
SungWook Choi <choisw950928@naver.com>
2022-02-12 14:25:29 +0900

    <Q10798 세로읽기(대기)> 22.02.12

---
+++ src/baekjoon/Q10798.java
@@ -0,0 +1,17 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Q10798 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < 5; i++) {
+            String str = scanner.nextLine();
+
+            for (int j = 0; j < str.length(); j++) {
+                char ch = str.charAt(i);
+            }
+        }
+    }
+}
