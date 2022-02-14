f36db1ab6a2f066f78113ab312d6e80f4966629a
SungWook Choi <choisw950928@naver.com>
2022-02-14 22:47:32 +0900

    <Q1316 그룹 단어 체커(fail)> 22.02.14

---
+++ src/review/Q1316_220214.java
@@ -0,0 +1,34 @@
+package review;
+
+import java.util.Scanner;
+
+public class Q1316_220214 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            String str = scanner.next();
+            boolean[] arr = new boolean[26];
+            boolean bol = true;
+
+            for (int j = 0; j < str.length(); j++) {
+                int ch = str.charAt(j) - 97;
+                if (arr[ch]) {
+                    if (str.charAt(j) != str.charAt(j - 1)) {
+                        bol = false;
+                        break;
+                    }
+                } else {
+                    arr[ch] = true;
+                }
+            }
+            if (bol) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
