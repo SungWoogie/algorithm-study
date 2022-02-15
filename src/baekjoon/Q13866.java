b302914cd7310b4bd3575b3bbf9c1ea7a140b93e
SungWook Choi <choisw950928@naver.com>
2022-02-15 16:33:35 +0900

    <Q13866 팀 나누기> 22.02.15 - Math.abs 를 활용하여 조금만 생각해보면 간단한 문제였다. - 4개의 값을 넣고 제일 작은 숫자와 제일 큰 수자를 더하고, 중간 값들을 더한뒤에 빼주면 되는 문제다. * 마이너스가 나올 수 있기 때문에 Math.abs 를 메소드를 사용하여 절대값으로 반환한다 즉, 음수를 양수로 만들어서 처리하는 것이다.

---
+++ src/baekjoon/Q13866.java
@@ -0,0 +1,15 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Q13866 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] arr = new int[4];
+        for (int i = 0; i < 4; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println(Math.abs((arr[0] + arr[3]) - (arr[1] + arr[2])));
+    }
+}
