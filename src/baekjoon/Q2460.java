629c2b5def66e64c819caf522dc5c76dcc194892
Woogie95 <choisw950928@naver.com>
2022-02-17 01:13:13 +0900

    <Q2460 지능형 기차-2> 22.02.17 - 반복적인 입력을 위하여 4번 반복하는 for 문을 만듬 - person 이란 변수를 만들고 person - out + in 을한다. - Math.max 메소드를 사용하여 큰값을 비교하고 max 를 출력한다.

---
+++ src/baekjoon/Q2460.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Q2460 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int person = 0;
+        int max = 0;
+
+        for (int i = 0; i < 10; i++) {
+            int out = scanner.nextInt();
+            int in = scanner.nextInt();
+
+            person = person - out + in;
+            max = Math.max(person, max);
+        }
+        System.out.println(max);
+    }
+}
