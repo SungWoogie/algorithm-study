ab319bdcd0bd18743b939a2af7fe9963472e4791
Woogie95 <choisw950928@naver.com>
2022-02-23 00:25:34 +0900

    <Q2587_ok 대표값2 22.02.23>

---
+++ src/baekjoon/Q2587.java
@@ -0,0 +1,20 @@
+package baekjoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Q2587 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] arr = new int[5];
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scanner.nextInt();
+            sum += arr[i];
+        }
+        Arrays.sort(arr);
+        System.out.println(sum / arr.length);
+        System.out.println(arr[2]);
+    }
+}
