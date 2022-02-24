0289783075dfc6494d28bb77c83b7fd1148d106a
Woogie95 <choisw950928@naver.com>
2022-02-24 17:32:38 +0900

    <Q2566 최대값(2차원)_o 22.02.24>

---
+++ src/baekjoon/Q2566.java
@@ -0,0 +1,27 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Q2566 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[][] arr = new int[9][9];
+
+        int max = 0;
+        int firstIndex = 0;
+        int lastIndex = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                arr[i][j] = scanner.nextInt();
+
+                if (arr[i][j] > max) {
+                    max = arr[i][j];
+                    firstIndex = i;
+                    lastIndex = j;
+                }
+            }
+        }
+        System.out.println(max);
+        System.out.println((firstIndex + 1) + " " + (lastIndex + 1));
+    }
+}
