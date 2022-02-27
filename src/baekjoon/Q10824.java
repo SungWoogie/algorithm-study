5479c16e070953be0cff3f10b9c3b622aa74f802
Woogie95 <choisw950928@naver.com>
2022-02-27 22:35:17 +0900

    <Q10824 네 수 22.02.27> - 입력 1,000,000 + 1,000,100 조심

--- src/baekjoon/Q10824.java
+++ src/baekjoon/Q10824.java
@@ -7,21 +7,21 @@ public class Q10824 {
         Scanner scanner = new Scanner(System.in);
         String[] strArr = new String[4];
 
-        String str = "";
-        String str1 = "";
+        String word = "";
+        String word1 = "";
         for (int i = 0; i < 4; i++) { // 입력 4개
             strArr[i] = scanner.next();
         }
         for (int i = 0; i < 4; i++) {
             if (i == 0 || i == 1) { // 0번째, 1번째 붙여
-                str = strArr[0] + strArr[1];
+                word = strArr[0] + strArr[1];
             }
             if (i == 2 || i == 3) {
-                str1 = strArr[2] + strArr[3];
+                word1 = strArr[2] + strArr[3];
             }
         }
-        long aValue = Long.parseLong(str);
-        long bValue = Long.parseLong(str1);
+        long aValue = Long.parseLong(word);
+        long bValue = Long.parseLong(word1);
         System.out.println(aValue + bValue);
     }
 }
