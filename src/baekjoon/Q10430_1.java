48d49cae2b8a7bbbcb089da5febefd694e1f22ef
Woogie95 <choisw950928@naver.com>
2022-03-01 16:22:40 +0900

    <Q10430 나머지_o 22.03.01>

---
+++ src/baekjoon/Q10430_1.java
@@ -0,0 +1,19 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+// 22.03.01
+public class Q10430_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+        System.out.println((a + b) % c);
+        System.out.println(((a % c) + (b % c)) % c);
+        System.out.println((a * b) % c);
+        System.out.println(((a % c) * (b % c)) % c);
+    }
+}
