1faa45f7cc997692bf0e58dc199db7fc45aae121
Woogie95 <choisw950928@naver.com>
2022-03-01 16:35:41 +0900

    <Q11654_1 아스키 코드_o 22.03.01>

---
+++ src/baekjoon/Q11654_1.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+// 22.03.01
+public class Q11654_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String str = scanner.next();
+        System.out.println((int) str.charAt(0));
+    }
+}
