89f46f8310811fa9f0d610f9a21de6bc24532561
Woogie95 <choisw950928@naver.com>
2022-03-01 14:48:28 +0900

    <Ex_Enum 22.03.01>

--- src/study/Enum2_220301.java
+++ src/study/Enum2_220301.java
@@ -19,6 +19,25 @@ enum kind {
     public String getSymbol() {
         return symbol;
     }
+
+    public static kind of(int kind) {
+        if (kind < 1 || kind > 4) {
+            throw new IllegalArgumentException("Invalid value :" + kind);
+        }
+        return kindArr[kind - 1];
+    }
+
+    public kind rotate(int num) {
+        num = num % 4;
+        if (num < 0) {
+            num += 4;
+        }
+        return kindArr[(value - 1 + num) % 4];
+    }
+
+    public String toString() {
+        return name() + getSymbol();
+    }
 }
 
 public class Enum2_220301 {
@@ -26,5 +45,16 @@ public class Enum2_220301 {
         for (kind a : kind.values()) {
             System.out.printf("%s = %d%n", a.name(), a.getValue());
         }
+
+        kind k1 = kind.EAST;
+        kind k2 = kind.of(3);
+
+        System.out.printf("d1 = %s, %d%n", k1.name(), k1.getValue());
+        System.out.printf("d2 = %s, %d%n", k2.name(), k2.getValue());
+        System.out.println(kind.EAST.rotate(1));
+        System.out.println(kind.EAST.rotate(2));
+        System.out.println(kind.EAST.rotate(-1));
+        System.out.println(kind.EAST.rotate(-2));
+
     }
 }
