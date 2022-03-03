66fd607b6baaa33eb81357576ffbcbd8fccb20b4
Woogie95 <choisw950928@naver.com>
2022-03-03 18:40:56 +0900

    <Q13752 히스토그램 22.03.03>

---
+++ src/baekjoon/Q13752.java
@@ -0,0 +1,20 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Q13752 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int num = scanner.nextInt();
+
+            for (int j = 0; j < num; j++) {
+                System.out.print("=");
+            }
+            System.out.println();
+        }
+    }
+}
