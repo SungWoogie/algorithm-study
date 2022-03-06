596570732eab877e9dc4a10dd7a8336bb1ae06b7
Woogie95 <choisw950928@naver.com>
2022-03-07 00:26:54 +0900

    <Q20053 최소, 최대2 22.03.06>

--- src/baekjoon/Q20053.java
+++ src/baekjoon/Q20053.java
@@ -1,21 +1,21 @@
 package baekjoon;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Q20053 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int t = Integer.parseInt(st.nextToken());
 
-//            int[] arr = new int[n];
+        int t = Integer.parseInt(br.readLine());
+
         for (int i = 0; i < t; i++) {
-            int n = Integer.parseInt(st.nextToken());
-            for (int j = 0; j < n; j++) {
+            int n = Integer.parseInt(br.readLine());
+            int[] arr = new int[n];
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < arr.length; j++) {
                 arr[j] = Integer.parseInt(st.nextToken());
             }
             Arrays.sort(arr);
@@ -23,3 +23,4 @@ public class Q20053 {
         }
     }
 }
+
