2c7bd6b4db276783bec9136d0b165087d97eb535
Woogie95 <choisw950928@naver.com>
2022-03-08 22:36:31 +0900

    <Q23795 사장님 도박은 재미로 하셔야 합니다(o) 22.03.08>

---
+++ src/baekjoon/Q23795.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Q23795 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int end = -1;
+        int sum = 0;
+        while (true) {
+            int num = scanner.nextInt();
+            if (num == end) {
+                break;
+            } else {
+                sum += num;
+            }
+        }
+        System.out.println(sum);
+    }
+}
