d7ae7bf208fc5274b6a0f4e1e20ed2e3223f188d
Woogie95 <choisw950928@naver.com>
2022-03-12 23:23:37 +0900

    <Q2743 단어 길이 재기 22.03.12>

---
+++ src/baekjoon/Q2743.java
@@ -0,0 +1,12 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Q2743 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String str = scanner.next();
+        System.out.println(str.length());
+    }
+}
