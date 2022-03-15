3a95f6c89a74ffac0332bf8d37e627d0fb682919
Woogie95 <choisw950928@naver.com>
2022-03-16 01:11:37 +0900

    <Q2577 숫자의 개수 22.03.16>

--- src/baekjoon/Q2577_1.java
+++ src/baekjoon/Q2577_1.java
@@ -1,15 +1,27 @@
 package baekjoon;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Q2577_1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int[] arr = new int[10];
 
         int a = scanner.nextInt();
         int b = scanner.nextInt();
         int c = scanner.nextInt();
 
         int multiply = a * b * c;
+
+        while (multiply > 0) {
+            int percent = multiply % 10;
+            multiply /= 10;
+            arr[percent]++;
+        }
+        for (int s : arr) {
+            System.out.println(s);
+        }
     }
 }
