e6f55fcffe1bd5ec02559b5cb448edef2fd5cd7a
Woogie95 <choisw950928@naver.com>
2022-03-16 19:54:45 +0900

    <Q10820 문자열 분석(f) 22.03.16> - scanner 로 활용 할때는 hasNextLine() 메소드 활용 - toCharArray 메소드 활용 - 아스키 코드를 조건으로 주어 할 수 있음

---
+++ src/baekjoon/Q10820.java
@@ -0,0 +1,32 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Q10820 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNextLine()) {
+            String str = scanner.nextLine();
+            int lower = 0; // 소문자
+            int upper = 0; // 대문자
+            int number = 0; // 숫자
+            int blank = 0; // 공백
+
+            for (int i = 0; i < str.length(); i++) {
+                char[] chArr = str.toCharArray();
+
+                if (chArr[i] >= 97 && chArr[i] <= 122) {
+                    lower++;
+                } else if (chArr[i] >= 65 && chArr[i] <= 90) {
+                    upper++;
+                } else if (chArr[i] >= 48 && chArr[i] <= 57) {
+                    number++;
+                } else {
+                    blank++;
+                }
+            }
+            System.out.println(lower + " " + upper + " " + number + " " + blank);
+        }
+    }
+}
