aef060c408045356463aa7c4fa1898fecc151957
Woogie95 <choisw950928@naver.com>
2022-03-17 23:10:34 +0900

    <Q10995 별찍기 - 20(f) 22.03.17>

--- src/baekjoon/Q10995.java
+++ src/baekjoon/Q10995.java
@@ -10,7 +10,7 @@ public class Q10995 {
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n * 2; j++) {
-                if ((i + j) % 2 == 0) {
+                if ((i + j) % 2 != 1) {
                     System.out.print("*");
                 } else {
                     System.out.print(" ");
