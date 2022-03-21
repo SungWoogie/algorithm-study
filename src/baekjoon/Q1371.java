725cf2fd85cda58d03fb3ff0b67b38af8603347f
Woogie95 <choisw950928@naver.com>
2022-03-21 23:56:30 +0900

    <Q2864 5와 6의 차이(f) 22.03.21>

---
+++ src/baekjoon/Q1371.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Q1371 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        whj
+                9
+    }
+}
