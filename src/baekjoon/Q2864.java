50fcc5733c0b5e90e89c34cba350264f66dde0e6
Woogie95 <choisw950928@naver.com>
2022-03-22 16:12:12 +0900

    <Q2864 5와 6의 차이(f) 22.03.22> - StringBuilder

--- src/baekjoon/Q2864.java
+++ src/baekjoon/Q2864.java
@@ -15,27 +15,27 @@ public class Q2864 {
         StringBuilder maxB = new StringBuilder();
 
         for (int i = 0; i < a.length(); i++) {
-            if (a.charAt(i) == '6') {
-                minA.append("5");
-            } else {
-                minA.append(a.charAt(i));
-            }
             if (a.charAt(i) == '5') {
                 maxA.append("6");
             } else {
                 maxA.append(a.charAt(i));
             }
+            if (a.charAt(i) == '6') {
+                minA.append("5");
+            } else {
+                minA.append(a.charAt(i));
+            }
         }
         for (int i = 0; i < b.length(); i++) {
-            if (b.charAt(i) == '6') {
-                minB.append("5");
-            } else {
-                minB.append(b.charAt(i));
-            }
             if (b.charAt(i) == '5') {
                 maxB.append("6");
             } else {
                 maxB.append(b.charAt(i));
+            }
+            if (b.charAt(i) == '6') {
+                minB.append("5");
+            } else {
+                minB.append(b.charAt(i));
             }
         }
         int min = Integer.parseInt(minA.toString()) + Integer.parseInt(minB.toString());
