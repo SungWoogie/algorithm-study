640d81bd4cbc59cfcab9d9fd71985c63338d540d
Woogie95 <choisw950928@naver.com>
2022-03-23 23:53:46 +0900

    <Q1371 가장 많은 글자(f) 22.03.23>

--- src/baekjoon/Q1371.java
+++ src/baekjoon/Q1371.java
@@ -5,9 +5,29 @@ import java.util.Scanner;
 public class Q1371 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
 
+        int[] arr = new int[26];
 
-        whj
-                9
+        while (scanner.hasNextLine()) {
+            String str = scanner.nextLine();
+
+            for (int i = 0; i < str.length(); i++) {
+                if (str.charAt(i) >= 'a' && str.charAt(i) <= 'z') {
+                    arr[str.charAt(i) - 'a']++;
+                }
+            }
+        }
+        int max = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (max < arr[i]) {
+                max = arr[i];
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (max == arr[i]) {
+                System.out.print((char) i + 97);
+            }
+        }
     }
 }
