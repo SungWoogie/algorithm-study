824dcb645a75b39e44e488fe4a2710bd6d3ce10d
Woogie95 <choisw950928@gmail.com>
2022-04-26 13:26:38 +0900

    test : 정수의 개수

---
+++ src/baekjoon/Q10821.java
@@ -0,0 +1,12 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Q10821 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] str = scanner.next().split(",");
+        System.out.println(str.length);
+    }
+}
