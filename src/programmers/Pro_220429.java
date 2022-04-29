5e2abddecb90e97206629b3f8e50f4f912f1f8d6
Woogie95 <choisw950928@gmail.com>
2022-04-29 14:19:58 +0900

    test : "문자열을 정수로 바꾸기"

---
+++ src/programmers/Pro_220429.java
@@ -0,0 +1,14 @@
+package programmers;
+
+public class Pro_220429 {
+    public static int solution(String str) {
+        return Integer.parseInt(str);
+    }
+
+    public static void main(String[] args) {
+
+        String str = "-1234";
+
+        System.out.println(solution(str));
+    }
+}
