7472723ecbd20985f3c2804d20948826469f6e6c
Woogie95 <choisw950928@gmail.com>
2022-05-30 14:01:09 +0900

    test: "약수의 개수와 덧셈"

--- src/programmers/Pro_220528.java
+++ src/programmers/Pro_220528.java
@@ -1,42 +1,25 @@
 package programmers;
 
-public class Pro_220528 {
+public class Pro_220528 {   // 약수의 개수와 덧셈
     public static int solution(int left, int right) {
-        int answer = 0;
-        int count = 0;
+        int answer = 0; // 리턴 할 값
 
-        for (int i = left; i < right; i++) {
-            count = 0;
-            for (int j = 1; j < i; j++) {
+        for (int i = left; i <= right; i++) {   // left ~ right 까지 반복
+            int count = 0;  // 약수 개수 세기 위한 저장공간
+            for (int j = 1; j <= i; j++) {  // 약수 구하는 로직
                 if (i % j == 0) {
                     count++;
                 }
             }
-            if (count % 2 == 0) {
+            if (count % 2 == 0) {   // 짝수면 값을 더해준다.
                 answer += i;
             } else {
-                answer += (i * -1);
+                answer -= i;    // 홀수면 값을 뺀다.
             }
         }
         return answer;
     }
 
-    public int solution2(int left, int right) {
-        int answer = 0;
-        int cnt = 0;
-        for (int i = left; i <= right; i++) {
-            cnt = 0;
-            for (int j = 1; j <= i; j++) {
-                if (i % j == 0) cnt++;
-            }
-            if (cnt % 2 == 0) {
-                answer += i;
-            } else {
-                answer += i * -1;
-            }
-        }
-        return answer;
-    }
 
     public static void main(String[] args) {
         int left = 24;
