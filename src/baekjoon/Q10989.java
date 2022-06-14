3634b9b79b8fcfa8ba6b9305d36e3d065f25b3ca
Woogie95 <choisw950928@gmail.com>
2022-06-14 22:24:50 +0900

    test: "수 정렬하기 3 f"

---
+++ src/baekjoon/Q10989.java
@@ -0,0 +1,24 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class Q10989 { // 수 정렬하기
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            sb.append(arr[i]).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
