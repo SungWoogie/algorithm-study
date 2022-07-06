69d9a5aa1c957a6ed1e6e98e529f6e3dd57391aa
Woogie95 <choisw950928@gmail.com>
2022-07-06 15:43:38 +0900

    test: "소수 만들기 Lv1"

--- src/programmers/Lv1_220705_1.java
+++ src/programmers/Lv1_220705_1.java
@@ -15,7 +15,6 @@ public class Lv1_220705_1 {
             for (int j = i + 1; j < nums.length; j++) {
                 for (int k = j + 1; k < nums.length; k++) {
                     int sum = nums[i] + nums[j] + nums[k];
-                    System.out.println(sum);
                     if (isMinority(sum)) {
                         answer++;
                     }
@@ -28,10 +27,8 @@ public class Lv1_220705_1 {
     }
 
     public boolean isMinority(int num) {
-        System.out.println("제곱근 : " + Math.sqrt(num));
         for (int i = 2; i <= Math.sqrt(num); i++) {
             if (num % i == 0) {
-                System.out.println("여기가 몇인가 ? : " + num % i);
                 return false;
             }
         }
