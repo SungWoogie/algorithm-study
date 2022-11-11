d00fe94785267b05436fca356eca1e7da55f83b9
Woogie95 <choisw950928@gmail.com>
2022-11-11 17:22:32 +0900

    test: ""

--- src/study/Split.java
+++ src/study/Split.java
@@ -1,40 +1,41 @@
 package study;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class Split {
     public static void main(String[] args) {
-        List<String> numbers = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
-        String num = scanner.nextLine();
+        String number = scanner.nextLine();
 
-//        for (int i = 0; i < num.length(); i++) {
-        numbers.add(Arrays.toString(num.split("")));
-//        }
+        validateBlank(number);
 
-        for (String a : numbers) {
-            System.out.print(a + " ");
-        }
 
-    }
-}
-
-class Split1 {
-    public static void main(String[] args) {
         List<String> numbers = new ArrayList<>();
-        Scanner scanner = new Scanner(System.in);
-        String[] number = scanner.nextLine().split("");
-
-        for (int i = 0; i < number.length; i++) {
-            numbers.add(number[i]);
+        for (int i = 0; i < number.length(); i++) {
+            numbers.add(String.valueOf(number.charAt(i)));
         }
+        validateLength(number);
+        System.out.println(numbers);
+    }
 
-        for (String a : numbers) {
-            System.out.println(a + " ");
+
+    private static boolean validateLength(String number) {
+        if (number.length() < 3) {
+            throw new IllegalArgumentException("입력해야 할 숫자 3개보다 작습니다.");
+        }
+        if (number.length() > 3) {
+            throw new IllegalArgumentException("입력해야 할 숫자 3개보다 큽니다.");
+        }
+        return false;
+    }
+
+    private static void validateBlank(String inputNumber) {
+        if (inputNumber.contains(" ")) {
+            throw new IllegalArgumentException("공백이 포함되어있습니다.");
         }
     }
+
 }
