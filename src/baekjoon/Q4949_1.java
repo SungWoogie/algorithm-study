cb384d5f7149e0c7518f648abf3604eadb3ee191
Woogie95 <choisw950928@gmail.com>
2023-01-05 19:03:49 +0900

    test : 균형잡힌 세상 stack

--- src/baekjoon/Q4949_1.java
+++ src/baekjoon/Q4949_1.java
@@ -3,38 +3,36 @@ package baekjoon;
 import java.util.Scanner;
 import java.util.Stack;
 
-public class Q4949_1 { // 균형잡힌 세상
+public class Q4949_1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        while (true) {
+        boolean isCommaInput = true;
+        while (isCommaInput) {
             Stack<Character> stack = new Stack<>();
             String str = scanner.nextLine();
-
             if (str.equals(".")) {
-                break;
-            }
-
-            for (int i = 0; i < str.length(); i++) {
-                if (str.charAt(i) == '(' || str.charAt(i) == '[') {
-                    stack.push(str.charAt(i));
-                } else {
-                    if (stack.isEmpty() || (stack.peek() == '(' && str.charAt(i) == ']') ||
-                            (stack.peek() == '[' && str.charAt(i) == ')')) {
+                isCommaInput = false;
+            } else {
+                for (int i = 0; i < str.length(); i++) {
+                    if (str.charAt(i) == '(' || str.charAt(i) == '[') {
                         stack.push(str.charAt(i));
-                        break;
-                    }
-                    if (!stack.isEmpty()) {
-                        if (stack.peek() == '(' && str.charAt(i) == ')' || stack.peek() == '[' && str.charAt(i) == ']') {
+                    } else {
+                        if (str.charAt(i) == ')' || str.charAt(i) == ']') {
+                            if (stack.isEmpty() || (stack.peek() == '(' && str.charAt(i) == ']')
+                                    || (stack.peek() == '[' && str.charAt(i) == ')')) {
+                                stack.push(str.charAt(i));
+                                break;
+                            }
                             stack.pop();
                         }
                     }
                 }
-            }
-            if (stack.isEmpty()) {
-                System.out.println("yes");
-            } else {
-                System.out.println("no");
+                if (stack.isEmpty()) {
+                    System.out.println("yes");
+                } else {
+                    System.out.println("no");
+                }
             }
         }
     }
