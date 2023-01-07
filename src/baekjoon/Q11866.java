a3a706bbd94ace1bed8d3b9596abfeef116b340c
Woogie95 <choisw950928@gmail.com>
2023-01-07 18:19:26 +0900

    test : 요세푸스 문제

--- src/baekjoon/Q11866.java
+++ src/baekjoon/Q11866.java
@@ -1,33 +1,26 @@
-//package baekjoon;
-//
-//import study.cloudstudy.circle.Circle;
-//
-//import java.util.ArrayList;
-//import java.util.LinkedList;
-//import java.util.List;
-//import java.util.Scanner;
-//
-//// 23.01.04 요세푸스 문제 0
-//public class Q11866 {
-//    public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//        int n = scanner.nextInt(); // 7
-//        int k = scanner.nextInt(); // 3
-//
-//        List<Integer> list = new ArrayList<>();
-//
-//        for (int i = 0; i < n; i++) { // 1,2,3,4,5,6,7
-//            list.add(i + 1);
-//        }
-//
-//        list.remove(1);
-//
-//        for (int i : list) {
-//            System.out.println(i);
-//        }
-//        System.out.println(list.size());
-//
-//        List<Integer> newList = new ArrayList<>();
-//
-//    }
-//}
+package baekjoon;
+
+import java.util.*;
+
+// 23.01.04 요세푸스 문제 0
+public class Q11866 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt(); // 7
+        int k = scanner.nextInt(); // 3
+
+        Queue<Integer> queue = new LinkedList<>(); // 큐 컬렉션 활용
+        for (int i = 1; i <= n; i++) {
+            queue.add(i); // 1 부터 7 까지의 값을 큐에 저장한다.
+        }
+        StringBuilder stringBuilder = new StringBuilder("<"); // 값 출력을 위한 빌더 생성
+        while (queue.size() > 1) { // 큐 사이즈가 1보다 클 때까지 반복한다.
+            for (int i = 0; i < k - 1; i++) { // (k-1) = 2 (3 보다 작을 때까지 첫번째 값을 맨뒤에 추가한다.)
+                queue.add(queue.poll());
+            }
+            stringBuilder.append(queue.poll()).append(", "); // 맨앞에 이란 값을 꺼내 빌더에 저장한다.
+        }
+        stringBuilder.append(queue.poll()).append(">"); // while 문이 종료되고 큐에 남아있는 마지막 값을 꺼내 빌더에 저장한다.
+        System.out.println(stringBuilder);
+    }
+}
