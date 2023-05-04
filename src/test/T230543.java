315c3d909dc0104b7c88df035d22fb025492ffaa
Woogie95 <choisw950928@gmail.com>
2023-05-04 13:46:21 +0900

    test: test

---
+++ src/test/T230543.java
@@ -0,0 +1,13 @@
+package test;
+
+import java.util.Random;
+
+public class T230543 {
+    public static void main(String[] args) {
+        Random random = new Random();
+
+
+        System.out.printf("%10d * %.0f%% = %10d\n",1000000000, 10.0, 10);
+
+    }
+}
